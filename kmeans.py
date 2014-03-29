835565386ba1b62dc7356b505a59b8eb56931701
Jared Forsyth <jared@jaredforsyth.com>
2014-03-29 16:20:34 -0600

    mostly done?

--- kmeans.py
+++ kmeans.py
@@ -22,12 +22,59 @@ from pandas import DataFrame
 from numpy import array
 import numpy as np
 import docopt
+import math
+
+def avg_ones(ones, t):
+    if t == 'numeric':
+        real = [x for x in ones if not np.isnan(x)]
+        if not real:
+            return np.nan
+        return sum(real)/len(real)
+    if t == 'nominal':
+        counts = {}
+        for i in ones:
+            if not i in counts:counts[i] = 1
+            else: counts[i] += 1
+        items = sorted(counts.items(), (lambda x,y:y[1]-x[1]))
+        if items[0][0] == '?' and len(items) > 1:
+            return items[1][0]
+        return items[0][0]
+    raise Exception("Invalid type?" + t)
+
+def avg_points(points, types):
+    return [avg_ones(ones, t) for ones, t in zip(zip(*points), types)]
+
+def distance(a, b, types):
+    total = 0
+    for m, n, t in zip(a,b,types):
+        if t == 'numeric':
+            if np.isnan(m) or np.isnan(n):
+                total += 1
+            else:
+                total += (m-n)**2
+        elif t == 'nominal':
+            if '?' in (m, n) or m != n:
+                total += 1
+        else:
+            raise Exception('unknown {} {} {}'.format(t, m, n))
+    return math.sqrt(total)
+
+def closest(inst, cents, types):
+    best = None
+    for ix, c in enumerate(cents):
+        dst = distance(c, inst, types)
+        # print dst
+        if best is None or dst < best[1]:
+            best = ix, dst
+    return best
 
 class KMeans:
-    def __init__(self, data, k, random):
+    def __init__(self, data, types, k, random, threshhold=0):
         self.data = data
         self.k = k
         self.random = random
+        self.threshhold = threshhold
+        self.types = types
         # init
         self.centroids = []
         if random:
@@ -35,31 +82,65 @@ class KMeans:
         else:
             ixs = data.index[:k]
         self.centroids = [data.loc[i] for i in ixs]
+        self.isse = self.regroup()
+        print 'initial  erro', self.isse
 
-    def step(self):
+    def regroup(self):
         groups = [[] for c in self.centroids]
+        sse = 0
+        assignments = []
         for i in self.data.index:
             p = self.data.loc[i]
-            best = None
-            for i, c in enumerate(self.centroids):
-                dst = distance(c, p)
-                if best is None or dst < best[1]:
-                    best = i, dst
+            best = closest(p, self.centroids, self.types)
+            # print i, best
             groups[best[0]].append(p)
+            assignments.append(best[0])
+            sse += best[1]**2
+        self.groups = groups
+        for i in range(0, len(assignments), 10):
+            print ' '.join('{}={}'.format(j,assignments[j]) for j in range(i,i+10) if j < len(assignments))
+        # print map(len, groups)
+        return sse
+
+    def step(self):
         news = []
         change = 0
-        for i, group in enumerate(groups):
-            newc = avg_points(group)
-            change += dst(self.centroids[i], newc)
+        for i, group in enumerate(self.groups):
+            print 'group', i, len(group)
+            # for n in group:print list(n)
+            newc = avg_points(group, self.types)
             news.append(newc)
-        self.centroids = map(avg_points, groups)
+            print newc
+        # print news
+        self.centroids = news
+        return self.regroup()
 
     def run(self):
-        while True:
+        sses = [self.isse]
+        for i in xrange(10000):
+            sses.append(self.step())
+            print 'Errpr', sses[-1]
+            if len(sses) > 4:
+                sses = sses[-4:]
+                if sses[-1] >= sses[0]:
+                    break
+        else:
+            print 'Maxed out the iterations'
+        return i
 
-def run_kmeans(fname, k=4, random=False):
+def run_kmeans(fname, k=5, random=False):
     data, meta = loadarff(fname)
     data = DataFrame(data)
-    mines = KMeans(data, k, random=random)
+    data = data[data.columns[1:-1]]
+    types = [meta[name][0] for name in meta.names()[1:-1]]
+    means = KMeans(data, types, k, random=random)
+    iters = means.run()
+    print iters
+    for c in means.centroids:
+        print c
+
+
+if __name__=='__main__':
+    run_kmeans('./laborWithID.arff')
 
 # vim: et sw=4 sts=4
