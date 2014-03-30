9b411b8038b4c1d5222e83c178803c658055558e
Jared Forsyth <jared@jaredforsyth.com>
2014-03-29 23:14:29 -0600

    experimenting, normalization, different distance metrics, etc. fully functional

--- hac.py
+++ hac.py
@@ -26,12 +26,13 @@ import math
 
 from utils import *
 
-def make_distances(data, types):
-    zeros = np.zeros((len(data), len(data)))
-    for i in xrange(len(data)):
-        for j in xrange(i+1, len(data)):
-            zeros[i,j] = zeros[j,i] = distance(data.iloc[i], data.iloc[j], types)
-    return zeros
+def read_dists(sz):
+    dists = np.zeros((sz, sz))
+    for i, lin in enumerate(open('./mx.txt').read().split('\n')):
+        for j, n in enumerate(lin.split(';')):
+            if not n:continue
+            dists[i,j] = float(n)
+    return dists
 
 class HAC:
     def __init__(self, data, types, single=True):
@@ -41,62 +42,109 @@ class HAC:
         self.clusters = [[a,] for a in range(len(data.index))]
 
         self.distances = make_distances(data, types)
+        # self.distances = read_dists(len(data))
+        # for line in self.distances:
+            # print ';'.join(map(str, line))
+        # fail
 
     def dist(self, one, two):
-        best = None
-        for a in one:
-            for b in two:
-                d = distance(self.data.iloc[a], self.data.iloc[b], self.types)
-                if best is None:
-                    best = d
-                elif (d < best if self.single else d > best):
-                    best = d
-        return best
+        return big_dist(one, two, self.single, self.distances)
 
     def step(self):
-        mx = np.empty((len(self.clusters), len(self.clusters)))
-        mx[:] = np.inf
+        # mx = np.empty((len(self.clusters), len(self.clusters)))
+        # mx[:] = np.inf
         best = None
         for i in xrange(len(self.clusters)):
             for j in xrange(i+1, len(self.clusters)):
                 d = self.dist(self.clusters[i], self.clusters[j])
                 if best is None or d < best[0]:
                     best = d, i, j
-        print 'merging', best[1], best[2], best[0]
+        print len(self.clusters), 'merging', best[1], best[2], best[0]
         self.clusters[best[1]] += self.clusters.pop(best[2])
 
     def serr(self):
         serrs = []
+        centroids = []
+        instances = []
         for ixs in self.clusters:
             points = [self.data.iloc[ix] for ix in ixs]
+            instances.append(points)
             centroid = avg_points(points, self.types)
+            centroids.append(centroid)
             sse = 0
             for point in points:
                 sse += distance(centroid, point, self.types)**2
             serrs.append(sse)
-        return sum(serrs), serrs
+        return sum(serrs), serrs, centroids, davies(zip(instances, centroids), self.types)
 
     def run(self, k=5):
-        while len(self.clusters) > 5:
+        while len(self.clusters) > k:
             self.step()
         
+        # print 'Single?', self.single
         return self.serr()
 
-def main():
-    pass
+def abalone(k, tok=None, single=True, norm=True):
+    data, meta = loadarff('./abalone500.arff')
+    data = DataFrame(data)
+    if norm:
+        normalize(data, meta)
+    types = [meta[name] for name in meta.names()]
+    hac = HAC(data, types, single)
+
+    sse, _, _, dav= hac.run(k)
+    print 'Atk,', k, sse, dav
+    if tok:
+        for i in range(k-1, tok-1, -1):
+            sse, _, _, dav = hac.run(i)
+            print i, ',', sse, ',', dav
+    print 'Single', single
+
+def iris(k, single=True, tok=None, outin=True):
+    data, meta = loadarff('./iris.arff')
+    data = DataFrame(data)
+    if not outin:
+        data = data[data.columns[:-1]]
+    types = [meta[name] for name in meta.names()]
+    if not outin:
+        types = types[:-1]
+    hac = HAC(data, types, single)
+
+    sse, _, _ = hac.run(k)
+    print 'Atk,', k, sse
+    if tok:
+        for i in range(k-1, tok-1, -1):
+            sse, _, _ = hac.run(i)
+            print i, ',', sse
+    print 'Single', single
+    if outin:
+        print 'Output class included'
+    # print 'err', sse
+    # for err, centroid, cluster in zip(errs, centroids, hac.clusters):
+        # print ','.join(map(str, centroid + [len(cluster), err]))
+
+def n1():
+    data, meta = loadarff('./sponge.arff')
+    data = DataFrame(data)
+    types = [meta[name] for name in meta.names()]
+    hac = HAC(data, types, False)
+    sse, errs, centroids = hac.run(4)
+    print 'err', sse
+    for err, centroid, cluster in zip(errs, centroids, hac.clusters):
+        print ','.join(map(str, centroid + [len(cluster), err]))
 
 def debug():
     data, meta = loadarff('./laborWithID.arff')
     data = DataFrame(data)
     data = data[data.columns[1:-1]]
     types = [meta[name] for name in meta.names()[1:-1]]
-    hac = HAC(data, types, False)
+    hac = HAC(data, types, True)
     err = hac.run(5)
     print 'err', err
     for c in hac.clusters:
         print c
 
 if __name__ == '__main__':
-    debug()
+    abalone(7, 2, True, True)
 
 # vim: et sw=4 sts=4
--- kmeans.py
+++ kmeans.py
@@ -41,7 +41,10 @@ class KMeans:
             ixs = data.index[:k]
         self.centroids = [data.loc[i] for i in ixs]
         self.isse = self.regroup()
-        print 'initial  erro', self.isse
+        # print 'initial  erro', self.isse
+
+    def davies(self):
+        return davies(zip(self.groups, self.centroids), self.types)
 
     def regroup(self):
         groups = [[] for c in self.centroids]
@@ -57,8 +60,8 @@ class KMeans:
             sses[best[0]] += best[1]**2
             sse += best[1]**2
         self.groups = groups
-        for i in range(0, len(assignments), 10):
-            print ' '.join('{}={}'.format(j,assignments[j]) for j in range(i,i+10) if j < len(assignments))
+        # for i in range(0, len(assignments), 10):
+            # print ' '.join('{}={}'.format(j,assignments[j]) for j in range(i,i+10) if j < len(assignments))
         # print map(len, groups)
         return sse, sses
 
@@ -70,7 +73,7 @@ class KMeans:
             # for n in group:print list(n)
             newc = avg_points(group, self.types)
             news.append(newc)
-            print ', '.join(str(x) for x in newc)
+            # print ', '.join(str(x) for x in newc)
         # print news
         self.centroids = news
         return self.regroup()
@@ -79,7 +82,7 @@ class KMeans:
         sses = [self.isse]
         for i in xrange(10000):
             sses.append(self.step())
-            print 'Errpr', sses[-1]
+            # print 'Errpr', sses[-1]
             if len(sses) > 4:
                 sses = sses[-4:]
                 if sses[-1][0] >= sses[0][0]:
@@ -99,6 +102,17 @@ def debug():
     for c in means.centroids:
         print c
 
+def n1():
+    data, meta = loadarff('./sponge.arff')
+    data = DataFrame(data)
+    types = [meta[name] for name in meta.names()]
+    means = KMeans(data, types, 4, False)
+    i, (sse, errs) = means.run()
+
+    print 'err', sse
+    for err, centroid, cluster in zip(errs, means.centroids, means.groups):
+        print ','.join(map(str, centroid + [len(cluster), err]))
+
 
 def run_kmeans(fname, k, random=False):
     data, meta = loadarff('./laborWithID.arff')
@@ -110,7 +124,90 @@ def run_kmeans(fname, k, random=False):
     for c in means.centroids:
         print c
 
+def kbest(data, types, k, num=10):
+    best = kme(data, types, k)
+    for i in range(num-1):
+        a, b, d = kme(data, types, k)
+        if d < best[2]:
+            best = a, b, d
+    return best
+
+def kme(data, types, k):
+    means = KMeans(data, types, k, True)
+    a, b = means.run()
+    for group in means.groups:
+        if not len(group):
+            # print 'null group'
+            return kme(data, types, k)
+    return a, b, means.davies()
+
+def run_one(fname, k, tok=None, norm=True, skip_last=False):
+    data, meta = loadarff(fname)
+    data = DataFrame(data)
+    if norm:
+        normalize(data, meta)
+    types = [meta[name] for name in meta.names()]
+
+    if skip_last:
+        data = data[data.columns[:-1]]
+        types = types[:-1]
+
+    _, (sse, _), dav = kbest(data, types, k)
+
+    print 'Atk,', k, ',', sse, dav
+    if tok:
+        for i in range(tok, k):
+            _, (sse, _), dav = kbest(data, types, i)
+            print i, ',', sse, dav
+    if skip_last:
+        print 'Skipping last'
+    if norm:
+        print 'Normalized'
+
+def abalone(k, tok=None, norm=False):
+    data, meta = loadarff('./abalone500.arff')
+    data = DataFrame(data)
+    if norm:
+        normalize(data, meta)
+    types = [meta[name] for name in meta.names()]
+
+    means = KMeans(data, types, k, True)
+    _, (sse, _) = means.run()
+
+    print 'Atk,', k, ',', sse, means.davies()
+    if tok:
+        for i in range(tok, k):
+            means = KMeans(data, types, i, True)
+            _, (sse, _) = means.run()
+            print i, ',', sse, means.davies()
+
+def iris(k, tok=None, outin=True):
+    data, meta = loadarff('./iris.arff')
+    data = DataFrame(data)
+    if not outin:
+        data = data[data.columns[:-1]]
+    types = [meta[name] for name in meta.names()]
+    if not outin:
+        types = types[:-1]
+
+    means = KMeans(data, types, k, True)
+    _, (sse, _) = means.run()
+
+    print 'Atk,', k, ',', sse
+    if tok:
+        for i in range(tok, k):
+            means = KMeans(data, types, i, True)
+            _, (sse, _) = means.run()
+            print i, ',', sse
+    if outin:
+        pass#print 'Output class included'
+    # print 'err', sse
+    # for err, centroid, cluster in zip(errs, centroids, hac.clusters):
+        # print ','.join(map(str, centroid + [len(cluster), err]))
+
+
 if __name__=='__main__':
-    debug()
+    # abalone(7, 2, norm=True)
+    run_one('./abalone500.arff', 7, 2, skip_last=True)
 
 # vim: et sw=4 sts=4
